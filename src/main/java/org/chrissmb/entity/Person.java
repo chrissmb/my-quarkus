e27c181ce97001cbd5abffd6afd2415231c33424
Christopher Monteiro <chrissantista@gmail.com>
2024-05-31 21:33:50 -0300

    unit test improve

--- src/main/java/org/chrissmb/entity/Person.java
+++ src/main/java/org/chrissmb/entity/Person.java
@@ -1,16 +1,10 @@
 package org.chrissmb.entity;
 
-import java.util.Date;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
@@ -20,6 +14,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Person extends PanacheEntity {
     private String name;
-    private Date birthday;
+    private LocalDate birthday;
     private Double height;
 }
--- src/test/java/org/chrissmb/PersonResourceTest.java
+++ src/test/java/org/chrissmb/PersonResourceTest.java
@@ -1,29 +1,59 @@
 package org.chrissmb;
 
+import io.quarkus.panache.mock.PanacheMock;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.chrissmb.entity.Person;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
 public class PersonResourceTest {
 
     @Test
     public void testGetAllPerson() {
+        List<Person> personList = instancePersonList();
+        PanacheMock.mock(Person.class);
+        Mockito.when(Person.<Person>listAll()).thenReturn(personList);
         given()
                 .when().get("/person")
-                .then().statusCode(200);
+                .then().statusCode(200).body("size()", is(2));
     }
 
     @Test
-    public void savePerson() {
+    public void testSavePerson() {
         Person person = Person.builder()
-                .name("Mock Person")
+            .name("Mock Person")
+            .birthday(LocalDate.of(2000, 1, 1))
+            .height(1.8)
+            .build();
+        given().body(person).contentType(ContentType.JSON)
+            .when().post("/person")
+            .then().statusCode(200)
+                .body("name", is("Mock Person"))
+                .body("birthday", is("2000-01-01"))
+                .body("height", is(1.8f))
+                .body("id", is(notNullValue()));
+    }
+
+    private List<Person> instancePersonList() {
+        Person person1 = Person.builder()
+                .name("John")
+                .height(1.8)
+                .birthday(LocalDate.of(1990, 01, 02))
                 .build();
-        given().body(person).contentType("application/json")
-                .when().post("/person")
-                .then().statusCode(200).body("name", is("Mock Person"));
+        Person person2 = Person.builder()
+                .name("Maria")
+                .height(1.63)
+                .birthday(LocalDate.of(1990, 01, 02))
+                .build();
+        return List.of(person1, person2);
     }
 }
